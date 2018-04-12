29968422b45ad9419ef5f6f2b8ec1fa848197199
Daniel Ruskin <daniel@druskin.co>
2018-04-12 09:05:15 -0400

    UML updates

--- src/TextActivationLossConfig.h
+++ src/TextActivationLossConfig.h
@@ -24,7 +24,6 @@ public:
 
 	static double evalCost(const Sentence& correct, const Sentence& predict);
 
-
 	static void setGradients(const TextRnn<TextActivationLossConfig>& network, int bptt_truncate, const Sentence& x, const Sentence& y, const arma::mat& saved_states, const arma::mat& outputs, std::unique_ptr<arma::mat>& out_dCdW, std::unique_ptr<arma::mat>& out_dCdU, std::unique_ptr<arma::mat>& out_dCdV);
 };
 
